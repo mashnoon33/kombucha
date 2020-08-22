12fd414fd245f5d579ca81dfb6d015eebfaccc3d
Mashnoon Ibtesum <mashnoon33@gmail.com>
2020-08-22 14:42:46 -0400

    Initial Commit

---
+++ kombucha/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  kombucha
+//
+//  Created by Mashnoon Ibtesum on 8/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kombucha/kombuchaApp.swift
@@ -0,0 +1,17 @@
+//
+//  kombuchaApp.swift
+//  kombucha
+//
+//  Created by Mashnoon Ibtesum on 8/22/20.
+//
+
+import SwiftUI
+
+@main
+struct kombuchaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
